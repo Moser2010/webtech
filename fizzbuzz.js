7f9b8cabfcaea4115971793de7b3445a6aa0edd3
moser2010 <cmoser351@gmail.co>
2015-09-02 14:41:55 +0000

    Updated from the issues

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,15 @@
-//Create	a	file	called	“fizzbuzz.js”,	which	does	the	following:
-/*1. Logs	the	numbers	from	1	to	100 to	the	console,	but	for	multiples	of	three	(3),	log	
-“Fizz”	instead	of	the	number,	and	for	the	multiples	of	five	(5)	print	“Buzz”.	For	
-numbers	which	are	multiples	of	both	three	and	five, print	“FizzBuzz”.*/
+
 function fizzbuzz() {
   for (var i = 1; i < 101; i++) {
       
     if ((i % 3 == 0) && (i % 5 == 0)) {
-      console.log('fizzbuzz');  
+      console.log('FIZZBUZZ');  
     }
     else if (i % 3 == 0) {
-      console.log("fizz");
+      console.log("FIZZ");
     }
     else if (i % 5 == 0) {
-      console.log("buzz");
+      console.log("BUZZ");
     }
     else {
       console.log(i);
@@ -22,4 +19,3 @@ function fizzbuzz() {
 
 fizzbuzz();
 
-//test change
--- temperature.js
+++ temperature.js
@@ -1,14 +1,4 @@
-//Create	a	file	called	"temperature.js",	which	does	the	following:
 
-/*a. Creates	a	new	variable	`Temperature`,	which	is	an	object	with	two	properties:
-– toFahrenheit:	A	function	which	takes	one	argument:	`Celsius`	(a	number),	
-calculates	the	equivalent	value	in	Fahrenheit,	and	returns	the	Fahrenheit	value.
-– toCelsius:	A	function	which	takes	one	argument:	`Fahrenheit`	(a	number),	
-calculates	the	equivalent	value	in	Celsius,	and	returns	the	Celsius	value.
-The	formula	for	converting	Celsius	to	Fahrenheit	is:
-		F	=	C	×	(⁹⁄₅)	+	32
-The	reverse	of	that	is:
-		C	=	(F	- 32)	×	(⁵⁄₉)*/
 
 var temperature = {
     toCelsius: function(t) {
@@ -19,43 +9,10 @@ var temperature = {
     }
 };
     
-
-/*
-
-function tester(t){
-    return t;
-}
-tester(5);
-
-var tester2 = function(t){
-    return t;
-};
-tester2(5);
-
-(function tester3(t){
-    return t;
-});
-*/
-
-/*b. Creates	a	new	variable	`tempToday`,	and	sets	the	value	to	82.	This	is	today's	
-temperature	in	Fahrenheit.*/
-
 var tempToday = 82;
-
-/*c. Creates	a	new	variable	`tempInCelsius`,	and	sets	its	value	equal	to	the	Celsius	
-equivalent,	rounded	to	the	nearest	whole	number.	
-The	Celsius	equivalent	should	be	calculated	by	calling	the	`toFahrenheit`	function	
-property	on	`Temperature`,	passing	it	`tempToday`.	Round	the	result	by	using	the	
-`Math.round()`	function.*/
-
-var tempInCelsius = Math.round(temperature.toFahrenheit(tempToday));
-/*d. Creates	a	new	variabe, `message`,	and	sets	its	value	equal	to	a	string	that	is	in	this	
-form:	
-“Today's	temperature	is	<tempToday>°F,	which	is	<tempInCelsius>°C.”*/
-
+var tempInCelsius = Math.round(temperature.toCelsius(tempToday));
 var message = "Today's temperature is " + tempToday + " F which is " + tempInCelsius + " C.";
 
-//e. Logs	`message`	to	the	console.
 console.log(message);
 
 
